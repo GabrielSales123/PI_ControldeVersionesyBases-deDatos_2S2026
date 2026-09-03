fdd43ec7ac411cb21ad6404c25471f0507951113
Gabriel Sales <gabrielandres.sales2007@gmail.com>
2026-09-03 11:34:09 -0600

    Estructuracion del proyecto

---
+++ SistemadeCalificaciondeCursos/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Prueba</h1>
+      <h2>Prueba 2</h2>
+    </>
+  )
+}
+
+export default App
